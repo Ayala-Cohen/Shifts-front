c024796ba9af372ce1d5ce7353ffc0b3fc04376b
Ayala-Cohen <ayalula2000@gmail.com>
2020-12-16 13:47:16 +0200

    creating components

---
+++ src/app/Components/constraints/constraints.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-constraints',
+  templateUrl: './constraints.component.html',
+  styleUrls: ['./constraints.component.css']
+})
+export class ConstraintsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
