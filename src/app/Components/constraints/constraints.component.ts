ebdb473bd9372a630df56355fb2d18eb6ba8d8d6
ayalula2000@gmail.com <ayalula2000@gmail.com>
2021-02-01 22:34:21 +0200

    מרכוז לדפים שיבוץ ואילוצים קבועים

--- src/app/Components/constraints/constraints.component.ts
+++ src/app/Components/constraints/constraints.component.ts
@@ -8,9 +8,15 @@ import { WardService } from 'src/app/Services/ward.service';
 })
 export class ConstraintsComponent implements OnInit {
   activity_days = [1, 2, 3, 4, 5, 6, 7]
+  shift_can_not =""
   constructor(private ward_service:WardService) { }
 
   ngOnInit(): void {
+  }
+  Update(name_ward)
+  {
+    this.shift_can_not=name_ward
+    debugger
   }
 
 }
